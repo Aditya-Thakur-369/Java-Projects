4838045ed902cca474a7d9dfedd56751602d5234
Aditya Thakur <93264532+Aditya336699@users.noreply.github.com>
2022-12-25 22:37:28 +0530

    Delete Dashboad.java

--- Dashboad.java
+++
@@ -1,9 +0,0 @@
-package ATM_Machine;
-
-public class Dashboad {
-    public static void main(String args[]){
-        Account adi = new Account("87897799","Aditya",0.0,"aditya@gmail.com","9923732984");
-        adi.DepositMoney(100.0);
-        adi.Withdraw(20.0);
-    }
-}
